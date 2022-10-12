7aff01b96a925331d03ea6de4baf14eb16814273
Bartosz Kozikowski <106819506+bartkozik@users.noreply.github.com>
2022-10-12 16:40:54 +0200

    Update settings.py

--- Warsztat/settings.py
+++ Warsztat/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-upr(-l8tvnqml*i&l*-^^(u*5ci4+mgnyg#qkhuk_6it8*_tse'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
